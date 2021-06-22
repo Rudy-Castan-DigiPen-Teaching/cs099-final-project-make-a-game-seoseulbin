d18e6e97064bf67ca04fbf8c9c59be3e7a977ec5
seoseulbin <80270808+seoseulbin@users.noreply.github.com>
2021-06-23 03:08:26 +0900

    Create screen_constants.js

---
+++ screen_constants.js
@@ -0,0 +1,25 @@
+// Name       : Seulbin Seo
+// Assignment : final_project_make_a_game
+// Course     : CS099
+// Spring 2021
+
+const MAIN_MENU = 0;
+const START_SCREEN_1_0 = 10;
+const STORY_SCREEN_2_0 = 20;
+const OPTIONS_SCREEN_3_0 = 30;
+
+const START_SCREEN_1_1 = 11;
+const START_SCREEN_1_2 = 12;
+const START_SCREEN_1_3 = 13;
+const START_SCREEN_1_1_1 = 111;
+const START_SCREEN_1_1_2 = 112;
+const START_SCREEN_1_2_1 = 121;
+const START_SCREEN_1_2_2 = 122;
+const START_SCREEN_1_1_1_1 = 1111;
+const START_SCREEN_1_1_1_2 = 1112;
+
+const STORY_SCREEN_2_1 = 21
+const STORY_SCREEN_2_2 = 22
+const STORY_SCREEN_2_3 = 23
+
+const FINISH_SCREEN = 9000
