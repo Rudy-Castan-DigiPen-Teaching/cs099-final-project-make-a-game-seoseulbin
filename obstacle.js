329c080fdbb66b776b2ae2a610e65d71b091af64
seoseulbin <80270808+seoseulbin@users.noreply.github.com>
2021-06-27 03:55:08 +0900

    Create obstacle.js

---
+++ obstacle.js
@@ -0,0 +1,39 @@
+// Name       : Seulbin Seo
+// Assignment : final_project_make_a_game
+// Course     : CS099
+// Spring 2021
+
+class OBSTACLE {
+  constructor(images_array) {
+    this.obstacle_index = round(random(0, images_array.length - 1));
+
+    this.position_x = random(0, width);
+    this.position_y = 0;
+
+    this.width = 70;
+    this.height = 70;
+
+    this.y_velocity = this.obstacle_index + 1;
+
+    this.image = images_array[this.obstacle_index];
+
+    this.hp = this.obstacle_index;
+  }
+
+  update() {
+    this.position_y += this.y_velocity;
+  }
+
+  draw() {
+    push();
+    imageMode(CENTER);
+    image(
+      this.image,
+      this.position_x,
+      this.position_y,
+      this.width,
+      this.height
+    );
+    pop();
+  }
+}
