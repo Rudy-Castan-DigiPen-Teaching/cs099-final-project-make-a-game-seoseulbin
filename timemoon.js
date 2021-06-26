d7eab5dc02dc8be4e169b34d1cff3dc7930f3dda
seoseulbin <80270808+seoseulbin@users.noreply.github.com>
2021-06-27 03:17:37 +0900

    Update timemoon.js

--- timemoon.js
+++ timemoon.js
@@ -4,21 +4,19 @@
 // Spring 2021
 
 class MOON {
-    constructor (moon_x, moon_y){
-        this.x = moon_x
-        this.y = moon_y
-        this.diameter = 250
-    }
-    update(){
+  constructor(moon_x, moon_y) {
+    this.x = moon_x;
+    this.y = moon_y;
+    this.diameter = 250;
+  }
+  update() {}
 
-    }
-
-    draw(){
-        push()
-        stroke(238,232,170)
-        strokeWeight(5)
-        fill(240,230,140)
-        ellipse(this.x, this.y, this.diameter, this.diameter *9/10)
-        pop()
-    }
+  draw() {
+    push();
+    stroke("gold");
+    strokeWeight(7);
+    fill(230, 190, 138);
+    ellipse(this.x, this.y, this.diameter, (this.diameter * 9) / 10);
+    pop();
+  }
 }
