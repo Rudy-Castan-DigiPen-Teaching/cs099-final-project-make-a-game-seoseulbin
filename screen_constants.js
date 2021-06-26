6b4a3fa3d784e677e0ab2c329d55c599e25e54e5
seoseulbin <80270808+seoseulbin@users.noreply.github.com>
2021-06-27 03:17:56 +0900

    Update screen_constants.js

--- screen_constants.js
+++ screen_constants.js
@@ -4,22 +4,15 @@
 // Spring 2021
 
 const MAIN_MENU = 0;
+
 const START_SCREEN_1_0 = 10;
-const STORY_SCREEN_2_0 = 20;
-const OPTIONS_SCREEN_3_0 = 30;
-
+const START_SCREEN_1_0_1 = 101;
 const START_SCREEN_1_1 = 11;
-const START_SCREEN_1_2 = 12;
-const START_SCREEN_1_3 = 13;
-const START_SCREEN_1_1_1 = 111;
-const START_SCREEN_1_1_2 = 112;
-const START_SCREEN_1_2_1 = 121;
-const START_SCREEN_1_2_2 = 122;
-const START_SCREEN_1_1_1_1 = 1111;
-const START_SCREEN_1_1_1_2 = 1112;
 
+const STORY_SCREEN_2_0 = 20;
 const STORY_SCREEN_2_1 = 21
 const STORY_SCREEN_2_2 = 22
-const STORY_SCREEN_2_3 = 23
+
+const OPTIONS_SCREEN_3_0 = 30;
 
 const FINISH_SCREEN = 9000
